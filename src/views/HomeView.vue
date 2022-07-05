fe2ff88cb26a37b098a897a5f500d17cb57259ec
丁海洋 <3131283031@qq.com>
2022-07-05 09:53:17 +0800

    feat: feat: feat: feat(根目录): feat(根目录): 配了cz prettierrc

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <span>123</span>
+    <span>123</span>
+    <span>123</span>
+    <span>123</span>
   </div>
 </template>
 
